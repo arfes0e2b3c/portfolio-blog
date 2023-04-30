4c1a0558e464dde7379acbbf09954f182fac10b5
arfes0e2b3c <webyata@gmail.com>
2023-04-30 11:51:11 +0900

    ベースファイルの追加

--- src/components/shared/Header/index.tsx
+++ src/components/shared/Header/index.tsx
@@ -1,3 +1,3 @@
 export const Header = () => {
-  return <p>[id.tsx]</p>
+  return <p>ヘッダー</p>
 }
---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next'
+import { BlogList } from '../../components/BlogList'
+
+// export default function Blog() {
+//   return <p>index.tsx</p>
+// }
+
+const Page: NextPage = () => {
+  return <BlogList />
+}
+
+export default Page
