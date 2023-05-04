082de4248d96c759c338abf1d0378adbb39d2439
arfes0e2b3c <webyata@gmail.com>
2023-05-04 13:33:53 +0900

    [add] google fontsの追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,24 @@
 import { Header } from '@/components/shared/Header'
 import type { AppProps } from 'next/app'
 import 'sanitize.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({
+  weight: '100',
+  subsets: ['cyrillic']
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <style jsx global>
+        {`
+          html {
+            font-family: ${inter.style.fontFamily};
+            font-weight: ${400};
+          }
+        `}
+      </style>
       <Header />
       <Component {...pageProps} />
     </>
