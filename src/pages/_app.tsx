399c14b5125434ac621e7dd55d05e3d3dbbd1d54
arfes0e2b3c <webyata@gmail.com>
2023-05-04 09:45:55 +0900

    [add] sanitize.cssの追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css'
 import { Header } from '@/components/shared/Header'
 import type { AppProps } from 'next/app'
+import 'sanitize.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
