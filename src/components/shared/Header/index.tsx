1c8c23d642f2af92edd8c0957bb6b4013cc5d200
arfes0e2b3c <webyata@gmail.com>
2023-05-04 16:21:37 +0900

    [add] ヘッダーを追加

--- src/components/shared/Header/index.tsx
+++ src/components/shared/Header/index.tsx
@@ -1,3 +1,41 @@
+import Image from 'next/image'
+import {
+  blogTitle,
+  blogTitleWrapper,
+  header,
+  leftHeader,
+  menuIcon,
+  rightHeader,
+} from './styles/header.css'
+import { HeaderModal } from './elements/HeaderModal'
+import { useState } from 'react'
+import Link from 'next/link'
+
 export const Header = () => {
-  return <p>ヘッダー</p>
+  const [isOpen, setIsOpen] = useState(false)
+  function toggleModal() {
+    setIsOpen(!isOpen)
+  }
+  return (
+    <header className={header}>
+      <div className={leftHeader}>
+        <Link href='/'>
+          <Image src='/yata_icon.jpg' alt='アイコン画像' width={80} height={80} />
+        </Link>
+        <Link className={blogTitleWrapper} href='/'>
+          <h1 className={blogTitle}>ARFES</h1>
+        </Link>
+      </div>
+      <div className={rightHeader} onClick={toggleModal}>
+        <Image
+          className={menuIcon}
+          src={isOpen ? '/close.svg' : '/menu_icon.svg'}
+          alt='ハンバーガーメニュー'
+          width={100}
+          height={100}
+        />
+      </div>
+      {isOpen && <HeaderModal />}
+    </header>
+  )
 }
