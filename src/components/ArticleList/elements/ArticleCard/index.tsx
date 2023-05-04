a1d4c5aed10d123a4502603656f71878c62515b5
arfes0e2b3c <webyata@gmail.com>
2023-05-04 11:35:15 +0900

    [add] 記事カードにスタイルを追加

--- src/components/ArticleList/elements/ArticleCard/index.tsx
+++ src/components/ArticleList/elements/ArticleCard/index.tsx
@@ -1,5 +1,13 @@
 import { Article } from '@/types'
 import { FC } from 'react'
+import {
+  articleCard,
+  articleCreatedAt,
+  articleImage,
+  articleImageContainer,
+  articleTitle,
+  articleTitleContainer
+} from './styles/articleCard.css'
 
 type ArticleCardProps = {
   article: Article
@@ -9,16 +17,18 @@ export const ArticleCard: FC<ArticleCardProps> = ({ article }) => {
   article.createdAt = formatCreatedAt(article.createdAt)
 
   return (
-    <div>
-      <div>
-        <div>
-          <p>{article.createdAt}</p>
-        </div>
-        <img src={article.eyecatch?.url} alt='' />
+    <div className={articleCard}>
+      <div className={articleImageContainer}>
+        <img className={articleImage} src={article.eyecatch?.url} alt='' />
+        <p className={articleCreatedAt}>{article.createdAt}</p>
       </div>
-      <div>
-        <h4>{article.title}</h4>
+      <div className={articleTitleContainer}>
+        <h4 className={articleTitle}>{article.title}</h4>
       </div>
     </div>
   )
+}
+
+function formatCreatedAt(createdAt: string): string {
+  return createdAt.slice(0, 4) + '/' + createdAt.slice(5, 7) + '/' + createdAt.slice(8, 10)
 }
---
+++ src/components/ArticleList/elements/ArticleCard/styles/articleCard.css.ts
@@ -0,0 +1,51 @@
+import { style } from '@vanilla-extract/css'
+
+const cardWidth = '700'
+const cardHeight = '450'
+
+export const articleCard = style({
+  width: cardWidth + 'px',
+  height: cardHeight + 'px',
+  margin: '20px',
+  boxShadow: '0px 0px 10px 10px rgba(0, 0, 0, 0.05)',
+  transition: '.2s',
+  cursor: 'pointer',
+  ':hover': {
+    boxShadow: '0px 0px 10px 10px rgba(0, 0, 0, 0.1)'
+  }
+})
+
+export const articleImageContainer = style({
+  position: 'relative',
+  width: '100%'
+})
+
+export const articleImage = style({
+  width: '100%',
+  height: 'calc(367.5% / ' + cardHeight + ')'
+})
+
+export const articleCreatedAt = style({
+  position: 'absolute',
+  top: '20px',
+  left: '20px',
+  padding: '5px 20px',
+  margin: '0',
+  backgroundColor: '#333',
+  color: 'white',
+  borderRadius: '30px'
+})
+
+export const articleTitleContainer = style({
+  display: 'flex',
+  alignItems: 'center',
+  width: '100%',
+  height: 'calc((1 - 367.5 / ' + cardHeight + ') * ' + cardHeight + 'px)'
+})
+
+export const articleTitle = style({
+  width: '100%',
+  margin: '0',
+  marginLeft: '10px',
+  fontWeight: 'lighter'
+})
