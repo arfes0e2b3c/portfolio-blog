7ca5156e1907825c821e9eef1d1d1d4c0ced6b92
arfes0e2b3c <webyata@gmail.com>
2023-05-06 01:36:13 +0900

    [update] _app.tsxのimportのorderを修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,11 @@
 import type { AppProps } from 'next/app'
 import 'sanitize.css'
 import { Inter, Noto_Sans_JP } from 'next/font/google'
+import Script from 'next/script'
 import { useEffect } from 'react'
 import { Header } from '@/components/shared/Header'
 import { ShadowHeader } from '@/components/shared/ShadowHeader'
 import 'zenn-content-css'
-import Script from 'next/script'
 
 const inter = Inter({
   weight: '100',
