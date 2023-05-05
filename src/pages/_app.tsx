aefec8a25a863f8f2b2964c596c31f296fd8f526
arfes0e2b3c <webyata@gmail.com>
2023-05-05 17:17:46 +0900

    [update] フォント設定の修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { Header } from '@/components/shared/Header'
 import type { AppProps } from 'next/app'
 import 'sanitize.css'
+import { Inter, Noto_Sans_JP } from 'next/font/google'
 import { useEffect } from 'react'
 import { Header } from '@/components/shared/Header'
 import { ShadowHeader } from '@/components/shared/ShadowHeader'
@@ -8,6 +9,11 @@ import { ShadowHeader } from '@/components/shared/ShadowHeader'
 const inter = Inter({
   weight: '100',
   subsets: ['cyrillic'],
+})
+
+const notoSansJP = Noto_Sans_JP({
+  weight: '100',
+  subsets: ['latin'],
 })
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -19,8 +25,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <style jsx global>
         {`
           html {
-            font-family: ${inter.style.fontFamily};
-            font-weight: ${400};
+            font-family: ${inter.style.fontFamily}, ${notoSansJP.style.fontFamily};
             color: #333;
           }
         `}
