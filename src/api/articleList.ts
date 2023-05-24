566af95f0a14ec2a8ab3d2bed9022ec356852222
arfes0e2b3c <webyata@gmail.com>
2023-05-24 23:40:12 +0900

    [update] 記事一覧で公開している記事のみ取得する様に変更

--- src/api/articleList.ts
+++ src/api/articleList.ts
@@ -3,7 +3,7 @@ import { ArticleResponse } from '@/types'
 
 export const fetchArticleList = async () => {
   const articleList = await initMicroCms()
-    .get<ArticleResponse>('articles')
+    .get<ArticleResponse>('articles?filters=isPublished[equals]true')
     .then((res) => {
       return res.data
     })
