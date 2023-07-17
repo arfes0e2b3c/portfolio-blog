27017c37ea264a90ad849fd2f7ff1bfc8c55042e
arfes0e2b3c <webyata@gmail.com>
2023-07-17 14:39:59 +0900

    refactor: デフォルトOGPの設定、設定の_document.tsxへの移行

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,15 +23,6 @@ const Home: NextPage<{ articleList: ArticleResponse }> = ({ articleList }) => {
     <>
       <Head>
         <title>Arfes&apos;s Portfolio & Blog</title>
-        <meta name='description' content="Arfes's Portfolio & Blog" />
-        <meta property='og:image' content={'/public/images/eyecatch_no-image.png'} />
-        <meta property='og:title' content="Arfes's Portfolio & Blog" />
-        <meta property='og:description' content="Arfes's Portfolio & Blog" />
-        <meta property='og:type' content='article' />
-        <meta property='twitter:title' content='Arfes' />
-        <meta property='twitter:card' content='summary_large_image' />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='icon' href='/favicon.ico' />
       </Head>
       <style jsx global>
         {`
