c002faf577cc744cb2cd4c76eebbe9d34ff8d820
arfes0e2b3c <webyata@gmail.com>
2023-08-26 21:34:17 +0900

    fix: 略歴からリンクを削除

--- src/components/Index/elements/ProfileBody/index.tsx
+++ src/components/Index/elements/ProfileBody/index.tsx
@@ -3,7 +3,6 @@ import Link from 'next/link'
 import {
   companyImage,
   companyImageContainer,
-  companyLink,
   companyName,
   historyItem,
   historyItemInner,
@@ -17,40 +16,34 @@ import {
 const histories = [
   {
     company: 'Yokohama National University',
-    href: 'https://www.ynu.ac.jp/',
     job: 'Faculty of Business Administration',
     period: '2021/04/01~',
   },
   {
     company: 'Lumos Programming Circle',
-    href: 'https://twitter.com/lumos_program',
     job: 'Member',
     period: '2021/11/21~',
     src: '/company_logos/lumos.png',
   },
   {
     company: 'Prince Hotels&Resorts',
-    href: 'https://www.princehotels.co.jp/',
     job: 'Waiter',
     period: '2022/02/01~2022/06/30',
   },
   {
     company: 'aidiot inc.',
-    href: 'https://aidiot.jp/',
     job: 'Frontend/Backend Engineer',
     period: '2022/07/01~2023/05/15',
     src: '/company_logos/aidiot.png',
   },
   {
     company: 'YUMEMI Inc.',
-    href: 'https://www.yumemi.co.jp/',
     job: 'Frontend Engineer',
     period: '2023/06/12~2023/06/16',
     src: '/company_logos/yumemi.svg',
   },
   {
     company: 'Cookpad Inc.',
-    href: 'https://info.cookpad.com/',
     job: 'Tech Workshop Course',
     period: '2023/07/01~2023/07/09',
     src: '/company_logos/cookpad.png',
@@ -70,7 +63,7 @@ export const ProfileBody = () => {
           {histories.map((history, index) => {
             return (
               <li className={historyItem} key={index}>
-                <Link className={[companyLink, historyItemInner].join(' ')} href={history.href}>
+                <div className={[historyItemInner].join(' ')}>
                   <div className={companyImageContainer}>
                     <Image
                       className={companyImage}
@@ -85,7 +78,7 @@ export const ProfileBody = () => {
                     <p className={job}>{history.job}</p>
                     <p className={period}>{history.period}</p>
                   </div>
-                </Link>
+                </div>
               </li>
             )
           })}
