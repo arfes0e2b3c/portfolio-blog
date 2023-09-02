87bd3cce8ea7c4dc743f2414e52583054a9212b0
arfes0e2b3c <webyata@gmail.com>
2023-09-02 11:48:42 +0900

    feat: 略歴にCARTAを追加

--- src/components/Index/elements/ProfileBody/index.tsx
+++ src/components/Index/elements/ProfileBody/index.tsx
@@ -55,29 +55,12 @@ const histories = [
     period: '2023/07/01~2023/07/09',
     src: '/company_logos/cookpad.png',
   },
-  // {
-  //   company: 'Cookpad Inc.',
-  //   job: 'Tech Workshop Course',
-  //   period: '2023/07/01~2023/07/09',
-  // },
-  // {
-  //   company: 'CARTA Holdings',
-  //   job: 'engineer',
-  //   period: '2023/08/07~2023/08/25',
-  // },
-  // {
-  //   company: 'Nikkei inc.',
-  //   job: 'engineer',
-  //   period: '2023/09/18~2023/09/22',
-  // },
-  //   company: 'Nikkei inc.',
-  //   job: 'engineer',
-  //   period: '2023/09/18~2023/09/22',
-  // },
-  //   company: 'Cookpad Inc.',
-  //   job: 'Tech Workshop Course',
-  //   period: '2023/07/01~2023/07/09',
-  // },
+  {
+    company: 'CARTA HOLDINGS',
+    job: 'Treasure',
+    period: '2023/08/07~2023/08/25',
+    src: '/company_logos/carta.png',
+  },
 ]
 
 export const ProfileBody = () => {
