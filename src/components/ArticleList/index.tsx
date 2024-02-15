a6d734a4e792ad5a1cd9e0f9618f83f8a8a86573
arfes0e2b3c <webyata@gmail.com>
2024-02-15 21:45:14 +0900

    refactor: categoryListを別コンポーネントに切り分け

--- src/components/ArticleList/index.tsx
+++ src/components/ArticleList/index.tsx
@@ -1,19 +1,11 @@
 import { useQuery } from 'react-query'
 import { ArticleCard } from './elements/ArticleCard'
-import {
-  articleListContainer,
-  articleListInner,
-  articleListTitle,
-  categoryIndex,
-  categoryList,
-  categoryListItem,
-  selected,
-} from './styles/articleList.css'
+import { articleListContainer, articleListInner, articleListTitle } from './styles/articleList.css'
 import { UseFetchArticleList } from '@/hooks/articleListHooks'
 import { Article, ArticleResponse } from '@/types'
 import { UseFetchCategories } from '@/hooks/categoryListHooks'
 import { useState } from 'react'
-import { assignInlineVars } from '@vanilla-extract/dynamic'
+import { CategoryList } from './elements/CategoryList'
 
 export const ArticleList = (props: { articleList: ArticleResponse }) => {
   const [selectedCategory, setSelectedCategory] = useState({ index: 0, id: '' })
@@ -53,35 +45,11 @@ export const ArticleList = (props: { articleList: ArticleResponse }) => {
   return (
     <div className={articleListInner}>
       <h2 className={articleListTitle}>ARTICLE</h2>
-      <div>
-        <ul
-          className={categoryList}
-          style={assignInlineVars({
-            [categoryIndex]: selectedCategory.index.toString(),
-          })}
-        >
-          <li
-            className={[categoryListItem, selectedCategory.id === '' ? selected : ''].join(' ')}
-            onClick={() => setSelectedCategory({ index: 0, id: '' })}
-          >
-            全て
-          </li>
-          {categories?.contents?.map((category, index) => {
-            return (
-              <li
-                className={[
-                  categoryListItem,
-                  selectedCategory.id === category.id ? selected : '',
-                ].join(' ')}
-                key={category.id}
-                onClick={() => setSelectedCategory({ index: index + 1, id: category.id })}
-              >
-                {category.name}
-              </li>
-            )
-          })}
-        </ul>
-      </div>
+      <CategoryList
+        categories={categories ?? { contents: [] }}
+        selectedCategory={selectedCategory}
+        setSelectedCategory={setSelectedCategory}
+      />
       <div className={articleListContainer}>
         {filteredArticleList?.articleList?.map((article: Article) => (
           <ArticleCard key={article.id} article={article} />
